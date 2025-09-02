5fa0ab8dd07ad5206a202ff814593caa98bfff08
Royal Tk <efemeroyal247@gmail.com>
2025-09-02 23:45:05 +0100

    Fixed the line-through functionality and changed the image cross to a react icon to boost website performance

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,43 +1,62 @@
-import './CSS/TodoItems.css'
-import tick from './assets/tick.png'
-import not_tick from './assets/not_tick.png'
-import cross from './assets/cross.png'
+import "./CSS/TodoItems.css";
+import tick from "./assets/tick.png";
+import not_tick from "./assets/not_tick.png";
+import PropTypes from "prop-types";
+import { FaTimes } from "react-icons/fa";
 
-const TodoItems = ({no, display, text, setTodos}) => {
-
-  const deleteTodo = (no)=>{
+const TodoItems = ({ no, display, text, setTodos }) => {
+  const deleteTodo = (no) => {
     let data = JSON.parse(localStorage.getItem("todos"));
-    data = data.filter((todo)=> todo.no!==no);
+    data = data.filter((todo) => todo.no !== no);
     setTodos(data);
-  }
+  };
 
-  const toggle = () =>{
+  const toggle = () => {
     let data = JSON.parse(localStorage.getItem("todos"));
-    for(let i = 0; i<data.length; i++)
-    {
-      if (data[i].no===no){
-        if (data[i].display===""){
-            data[i].display = "line-through";
-        }
-        else{
-            data[i].display = "";
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].no === no) {
+        if (data[i].display === "") {
+          data[i].display = "line-through";
+        } else {
+          data[i].display = "";
         }
         break;
       }
     }
     setTodos(data);
-  }
+  };
 
   return (
-    <div className='todoitems'>
-        <div className={`todoitems-container${display}`} onClick={()=>{toggle(no)}}>
-            {display===""?<img src={not_tick} alt='' />:
-            <img src={tick} alt='' /> }
-            <div className="todoitems-text">{text}</div>
-        </div>
-        <img className='todoitems-cross-icon' onClick={()=>{deleteTodo(no)}} src={cross} alt=''/>
-    </div>
-  )
-}
+    <div className="todoitems">
+      <div
+        className={`todoitems-container `}
+        onClick={() => {
+          toggle(no);
+        }}
+      >
+        {display === "" ? (
+          <img src={not_tick} alt="" />
+        ) : (
+          <img src={tick} alt="" />
+        )}
+        <div className={`todoitems-text ${display}`}>{text}</div>
+      </div>
 
-export default TodoItems
+      <FaTimes
+        className="todoitems-cross-icon"
+        onClick={() => {
+          deleteTodo(no);
+        }}
+      />
+    </div>
+  );
+};
+
+export default TodoItems;
+
+TodoItems.propTypes = {
+  no: PropTypes.number.isRequired,
+  display: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  setTodos: PropTypes.func.isRequired,
+};
